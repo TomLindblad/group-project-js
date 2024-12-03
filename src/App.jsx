2e89a399fbf6f64459ab6d6062b536cfb6058f1a
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2024-12-03 17:05:09 +0100

    First version of the game added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Startpage from './pages/startpage.jsx';
-import Game from './pages/gamepage.jsx';
+import Gamepage from './pages/gamepage.jsx';
 
 function App() {
 
     return(
         <>
             <Startpage/>
-            <Game/>
+            <Gamepage/>
         </>
     );
 }
